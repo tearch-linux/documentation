4c685e6a277ca97de9b28ce2b9ba97887c945deb
Efe Çetin <efectn@6tel.net>
2020-09-09 20:45:25 +0300

    Fix GH

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
+
 
 # -- Project information -----------------------------------------------------
 
@@ -28,17 +28,9 @@ author = 'Muhammed Efe Çetin'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
-    "releases",
-    "sphinx_sitemap",
-    "recommonmark"
+    'sphinx_rtd_theme',
+    'sphinx.ext.githubpages',
 ]
-
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
-}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -67,3 +59,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Extension configuration -------------------------------------------------
